07d6b7525b833b4dceb5be85b7bcbfbc3aef22a4
FluffiiFluffs <FluffiiFluffs@Gmail.Com>
2025-11-24 07:19:45 -0600

    UI Started
    Loaded in a lot of placeholder images
    Edited theme for balloon.tscn to use the new ninepatch.
    Created game_state (GameState) global so (hopefully) elements do not interfere with each other.

---
+++ _globals/game_state.gd
@@ -0,0 +1,21 @@
+##game_state.gd
+##global script GameState
+extends Node2D
+##Keeps track of the game state so elements do not interfere with each other.
+
+
+@export_enum(
+	"STARTMENU0",
+	"FIELD1",
+	"BATTLE2",
+	"GAMEMENU3",
+	"INTERACTING4",
+	"CUTSCENE5",
+	"PAUSEMENU6",
+	"TRANSITION7",
+	"LOADING8",
+	"GAMEOVER9",
+	"SHOP10",
+	"DIALOGUE11"
+)
+var game_state: int = 0
