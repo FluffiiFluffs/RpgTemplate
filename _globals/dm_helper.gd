a561ad63f765580bdf3ff399ce1444ddeeedac9e
FluffiiFluffs <FluffiiFluffs@Gmail.Com>
2026-02-28 05:37:55 -0600

    Dialogue Manager Update Begin
    Added portraits / moods for warrior, thief, mage, healer
    Highly updated SpeakerResource. This was to support the coming changes to the way authoring is done within dialogue manager.
    
    Planned refactor for dialogue system
    Found out that the newest version of dialogue manager is probably needed due to the project being 4.6
    This update is to mark the point BEFORE the update to dialogue manager 3.10.1 from 3.9.0

--- _globals/dm_helper.gd
+++ _globals/dm_helper.gd
@@ -1,4 +1,54 @@
+## global script "DM", dm_helper.gd
 extends Node
+## Contains additional functionalty data and functions for dialogue manager plugin
 
+const SND_TXTBER = preload("uid://dr3xptfehm82e")
+
+## An array of all speakers within the game. Propagated upon game load
+var all_speakers : Array[SpeakerResource] = []
+
+## The default voice to be used if no voice file is set for a speaker resource
+var default_voice : AudioStream = SND_TXTBER
+
+
+## The current speaker
+var current_speaker : SpeakerResource = null
+var current_speaker_portrait_open : Texture2D = null
+var current_speaker_portrait_close : Texture2D = null
+
+
+
+
+
+## This is probably going to be defunct soon
 func set_portrait(speaker:String, expression: String)->void:
 	DialogueManager._set_portrait(speaker, expression)
+
+
+func find_speaker_by_id(id : String)->SpeakerResource:
+	for speaker in all_speakers:
+		if id == speaker.speaker_id:
+			return speaker
+	return null
+	
+func get_speaker_mood_open():
+	pass
+	
+func get_speaker_mood_closed():
+	pass
+
+func get_speaker_voice():
+	pass
+	
+func get_speaker_voice_pitch_min():
+	pass
+	
+func get_speaker_voice_pitch_max():
+	pass
+	
+func get_pronoun_by_id():
+	pass
+	
+func get_pronoun_type():
+	pass
+	
--- dialogue/speaker_resources/speaker_resource.gd
+++ dialogue/speaker_resources/speaker_resource.gd
@@ -1,23 +1,59 @@
 class_name SpeakerResource
 extends Resource
+## Defines the name, portraits, and voices to be used within the dialogue system 
 
-##Name of the speaker
-@export var name : String = ""
-##Portrait texture for normal emotion
-@export var normal : Texture2D
-##Portrait texture for talking (open mouth)
-@export var talk : Texture2D
-##Portrait texture for sad emotion
-@export var sad : Texture2D
-##Portrait texture for surprised emotion
-@export var surprise: Texture2D
-##Portrait texture for angry emotion
-@export var angry : Texture2D
-##Portrait texture for happy emotion
-@export var happy : Texture2D
-##Portrait texture for special emotion
-@export var special : Texture2D
-##Portrait texture for tired emotion
-@export var tired : Texture2D
-##Voice sound
+enum GENDER {HE, SHE, IT, THEY}
+
+@export_category("Identification")
+## ID of the speaker to be matched within the dialogue system
+@export var speaker_id : String = ""
+## Name displayed during talking within the dialogue system
+@export var display_name : String = ""
+## What pronoun set to use for this speaker.
+## Called in dialogue manager with do pronoun(speaker_id, pronoun_type).
+## Pronoun type: subject, object, possissive determiner, possesseive pronoun, reflexive.
+@export var pronoun : GENDER = GENDER.HE
+
+@export_category("Voice")
+## Voice sound for the speaker
 @export var voice : AudioStream
+## Should always be equal to or lower than pitch_min
+@export var pitch_min : float = 0.0
+## Should always equal to or higher than pitch_min
+@export var pitch_max : float = 0.0
+
+@export_category("Portrait")
+## Portrait texture for angry emotion (close mouth)
+@export var angry_close : Texture2D
+## Portrait texture for angry emotion (open mouth)
+@export var angry_open : Texture2D
+
+## Portrait texture for happy emotion (close mouth)
+@export var happy_close : Texture2D
+## Portrait texture for happy emotion (open mouth)
+@export var happy_open : Texture2D
+
+## Portrait texture for normal emotion (close mouth)
+@export var normal_close : Texture2D
+## Portrait texture for normal emotion (open mouth)
+@export var normal_open : Texture2D
+
+## Portrait texture for sad emotion (open mouth)
+@export var sad_close : Texture2D
+## Portrait texture for sad emotion (close mouth)
+@export var sad_open : Texture2D
+
+## Portrait texture for special emotion (close mouth)
+@export var special_close : Texture2D
+## Portrait texture for special emotion (open mouth)
+@export var special_open : Texture2D
+
+## Portrait texture for surprised emotion (close mouth)
+@export var surprise_close: Texture2D
+## Portrait texture for surprised emotion (open mouth)
+@export var surprise_open : Texture2D
+
+## Portrait texture for tired emotion (close mouth)
+@export var tired_close : Texture2D
+## Portrait texture for tired emotion (open mouth)
+@export var tired_open : Texture2D
